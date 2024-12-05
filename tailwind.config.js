9038e8d37b1be20a6365cbe0247d2eed5075b6c3
Bea Smith <bsmith152@ewu.edu>
2024-12-05 17:49:50 +0000

    updated fonts and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
       
         'hero': "url('https://images.alphacoders.com/527/thumb-1920-527845.jpg')",
         
-    },fontFamily:{Rubik: ['Rubik Glitch', 'system-ui']}
+    },fontFamily:{Rubik: ['Rubik Glitch', 'system-ui'], RedHat: ['Red Hat Display', 'sans-serif']}
+
   },
   plugins: [],
 }
